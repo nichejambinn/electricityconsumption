f0f6c328050ec304c33939ad6d0aae66e15bcabf
nichejambinn <benjaminrobininch@gmail.com>
2021-12-08 01:37:35 -0500

    Hadoop ls too slow, found workaround with date for monthly dirs

--- flume/latest-week.sh
+++ flume/latest-week.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ $1 != "" ]]; then 
+    echo "Creating test environment"
     hadoop fs -rm -r /test
     touch {a..d}.log
     hadoop fs -mkdir -p /test/2015/04/17 /test/2016/09/2{7..8} /test/2021/12/0{5..7} /test/2019/11/02
@@ -20,27 +21,21 @@ last_week=$(date -d "last week 13:00" '+%s')
 # iterate through folders in descending order
 for dir in $( hadoop fs -ls -R ${LATEST_PATH} | grep ^d | awk '{print $NF}' | sort -r )
 do
-    # remove directory if empty
-    if [[ $( hadoop fs -ls ${dir} | wc -l ) == 0 ]]; then
-        hadoop fs -rmdir ${dir}
-        echo "Deleted ${dir}"
+    # format path as datelike string YYYY-- YYYY-mm- YYYY-mm-dd
+    date_string=$( echo ${dir} | sed -r "s|${LATEST_PATH}\/([0-9]{4})\/?([0-9]{2})?\/?([0-9]{2})?|\1-\2-\3|" )
+
+    # get date in seconds from string
+    if [[ ${date_string} =~ [0-9]{1}-$ ]]; then
+        # monthly directories are set to last day of that month
+        dir_date=$(date -d "${date_string}01 +1 month -1 day" '+%s')
     else
-        # format path as datelike string YYYY-- YYYY-mm- YYYY-mm-dd
-        date_string=$( echo ${dir} | sed -r "s|${LATEST_PATH}\/([0-9]{4})\/?([0-9]{2})?\/?([0-9]{2})?|\1-\2-\3|" )
-
-        # if non-empty monthly directory continue on to next folder
-        if [[ ${date_string} =~ [0-9]{1}-$ ]]; then
-            continue
-        fi
-
-        # get date in seconds from string
         # year is parsed as today's date from that year
+        # date is parsed as expected
         dir_date=$(date -d ${date_string} '+%s')
+    fi
 
-        # if folder is from before last week remove it
-        if [[ ${dir_date} -lt ${last_week} ]]; then
-            hadoop fs -rm -r ${dir}
-            #echo "${dir} and contents removed"
-        fi
+    # if folder is from before last week remove it
+    if [[ ${dir_date} -lt ${last_week} ]]; then
+        hadoop fs -rm -r ${dir}
     fi
 done
