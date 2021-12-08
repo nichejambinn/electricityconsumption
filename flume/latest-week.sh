0f43d72672a20ef037612c6155ac78a087ac08e1
nichejambinn <benjaminrobininch@gmail.com>
2021-12-08 00:26:20 -0500

    Hadoop version of script - untested!!

--- flume/latest-week.sh
+++ flume/latest-week.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-mkdir -p 2021/12/04 2015/11/05 2019/12/29 2021/12/03
-touch 2021/12/04/a.log
-touch 2015/11/05/b.log
+if [[ $1 != "" ]]; then 
+    echo "Path in hdfs required"
+    exit 1
+else
+    LATEST_PATH=$1
+fi
 
 # delete all but last 7 days of logs
-#last_week=$(date -d "last week 13:00" '+%Y-%m-%d')
 last_week=$(date -d "last week 13:00" '+%s')
 
-#`hadoop fs -ls /path/to/directory | awk '{print $NF}' | grep .csv$ | tr '\n' ' '`
-
-#'hadoop fs -ls -R /ecoFlume/latest | grep '^d' | sort -r'
 # iterate through folders in descending order
-for dir in $(find . -type d | sort -r | grep [0-9]$ )
+for dir in $( hadoop fs -ls -R ${LATEST_PATH} | grep ^d | awk '{print $NF}' | sort -r )
 do
     # remove directory if empty
     if [[ $(ls -A ${dir} | wc -l) == 0 ]]; then
-        rmdir ${dir}
-        echo "${dir} removed"
+        hadoop fs -rmdir ${dir}
+        echo "Deleted ${dir}"
     else
         # format path as datelike string YYYY-- YYYY-mm- YYYY-mm-dd
-        date_string=$(echo ${dir} | sed -r 's|\.\/([0-9]{4})\/?([0-9]{2})?\/?([0-9]{2})?|\1-\2-\3|')
+        date_string=$(echo ${dir} | sed -r "s|${LATEST_PATH}([0-9]{4})\/?([0-9]{2})?\/?([0-9]{2})?|\1-\2-\3|")
 
         # if non-empty monthly directory continue on to next folder
         if [[ ${date_string} =~ [0-9]{1}-$ ]]; then
@@ -28,12 +27,13 @@ do
         fi
 
         # get date in seconds from string
+        # year is parsed as today's date from that year
         dir_date=$(date -d ${date_string} '+%s')
 
         # if folder is from before last week remove it
         if [[ ${dir_date} -lt ${last_week} ]]; then
             rm -r ${dir}
-            echo "${dir} and contents removed"
+            #echo "${dir} and contents removed"
         fi
     fi
 done
